ede5d8681d7a23e71225fdcf0a349b848104421a
kmullens1 <kmullens1@knights.ucf.edu>
2021-06-17 01:38:03 -0400

    Instructions Added into file

--- src/main/java/oop/example/Ex41.java
+++ src/main/java/oop/example/Ex41.java
@@ -2,6 +2,39 @@ package oop.example;
 
 public class Ex41 {
     public static void main(String[] args) {
+    //Pseudo Code
+    //Code
+    //Multiple Classes
+    //UML Class Diagrams
+    //Test Cases
+
+
+        //Name Sorter
+        /*Alphabetizing the contents of a file, or sorting its contents, is a great way
+        to get comfortable manipulating a file in your program.
+
+                Create a program that reads in the following list of names:
+            Ling, Mai
+            Johnson, Jim
+            Zarnecki, Sabrina
+            Jones, Chris
+            Jones, Aaron
+            Swift, Geoffrey
+            Xiong, Fong
+
+        Read this program and sort the list alphabetically. Then print the sorted list to a file that looks like thefollowing example output.
+        Example Output
+            Total of 7 names
+            -----------------
+            Ling, Mai
+            Johnson, Jim
+            Jones, Aaron
+            Jones, Chris
+            Swift, Geoffrey
+            Xiong, Fong
+            Zarnecki, Sabrina
+        Constraint
+            Don't hard-code the number of names.*/
 
     }
 }
--- src/main/java/oop/example/Ex42.java
+++ src/main/java/oop/example/Ex42.java
@@ -6,6 +6,33 @@ package oop.example;
 
 public class Ex42 {
     public static void main(String[] args) {
+        //Parsing a Data File
+        /*Sometimes data comes in as a structured format that you have to break
+        down and turn into records so you can process them. CSV, or comma-separated
+        values, is a common standard for doing this.
+        Construct a program that reads in the following data file:
+            Ling,Mai,55900
+            Johnson,Jim,56500
+            Jones,Aaron,46000
+            Jones,Chris,34500
+            Swift,Geoffrey,14200
+            Xiong,Fong,65000
+            Zarnecki,Sabrina,51500
+        Process the records and display the results formatted as a table, evenly spaced, as shown in theexample output.
+        Example Output
+            Last First Salary
+            --------------------------
+            Ling Mai 55900
+            Johnson Jim 56500
+            Jones Aaron 46000
+            Jones Chris 34500
+            Swift Geoffrey 14200
+            Xiong Fong 65000
+            Zarnecki Sabrina 51500
+        Constraints
+            Write your own code to parse the data. Don't use a CSV parser.
+            Use spaces to align the columns.
+            Make each column one space longer than the longest value in the column.*/
 
     }
 }
